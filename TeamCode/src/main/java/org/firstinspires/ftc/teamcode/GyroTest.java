87fac3934842f16d8ae09b21b5dcea801d41e5d8
nimcd <nickmcdonough@aol.com>
2017-08-25 16:14:55 -0400

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroTest.java
@@ -0,0 +1,185 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IntegratingGyroscope;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+
+@TeleOp(name = "GyroTest", group = "SensorTests")
+@Disabled
+public class GyroTest extends LinearOpMode {
+
+    public DcMotor leftMotor   = null;
+    public DcMotor  rightMotor  = null;
+    public DcMotor  autobroom    = null;
+    public Servo collector   = null;
+
+    public static final double MID_SERVO       =  0.5 ;
+    public static final double ARM_UP_POWER    =  0.45 ;
+    public static final double ARM_DOWN_POWER  = -0.45 ;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftMotor   = hwMap.dcMotor.get("left_drive");
+        rightMotor  = hwMap.dcMotor.get("right_drive");
+        autobroom    = hwMap.dcMotor.get("autobroom");
+        leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+
+        // Set all motors to zero power
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+        autobroom.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        autobroom.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Define and initialize ALL installed servos.
+        collector = hwMap.servo.get("left_hand");
+        collector.setPosition(MID_SERVO);
+    }
+
+    /***
+     *
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs  Length of wait cycle in mSec.
+     */
+    public void waitForTick(long periodMs) {
+
+        long  remaining = periodMs - (long)period.milliseconds();
+
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0) {
+            try {
+                Thread.sleep(remaining);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        // Reset the cycle clock for the next pass.
+        period.reset();
+    }
+
+    IntegratingGyroscope gyro;
+    ModernRoboticsI2cGyro modernRoboticsI2cGyro;
+
+    // A timer helps provide feedback while calibration is taking place
+    ElapsedTime timer = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        boolean lastResetState = false;
+        boolean curResetState  = false;
+
+        // Get a reference to a Modern Robotics gyro object. We use several interfaces
+        // on this object to illustrate which interfaces support which functionality.
+        modernRoboticsI2cGyro = hardwareMap.get(ModernRoboticsI2cGyro.class, "gyro");
+        gyro = (IntegratingGyroscope)modernRoboticsI2cGyro;
+        // If you're only interested int the IntegratingGyroscope interface, the following will suffice.
+        // gyro = hardwareMap.get(IntegratingGyroscope.class, "gyro");
+        // A similar approach will work for the Gyroscope interface, if that's all you need.
+
+        // Start calibrating the gyro. This takes a few seconds and is worth performing
+        // during the initialization phase at the start of each opMode.
+        telemetry.log().add("Gyro Calibrating. Do Not Move!");
+        modernRoboticsI2cGyro.calibrate();
+
+        // Wait until the gyro calibration is complete
+        timer.reset();
+        while (!isStopRequested() && modernRoboticsI2cGyro.isCalibrating())  {
+            telemetry.addData("calibrating", "%s", Math.round(timer.seconds())%2==0 ? "|.." : "..|");
+            telemetry.update();
+            sleep(50);
+        }
+        telemetry.log().clear(); telemetry.log().add("Gyro Calibrated. Press Start.");
+        telemetry.clear(); telemetry.update();
+
+        // Wait for the start button to be pressed
+        waitForStart();
+        telemetry.log().clear();
+        telemetry.log().add("Press A & B to reset heading");
+
+        // Loop until we're asked to stop
+        while (opModeIsActive())  {
+
+            // If the A and B buttons are pressed just now, reset Z heading.
+            curResetState = (gamepad1.a && gamepad1.b);
+            if (curResetState && !lastResetState) {
+                modernRoboticsI2cGyro.resetZAxisIntegrator();
+            }
+            lastResetState = curResetState;
+
+            // The raw() methods report the angular rate of change about each of the
+            // three axes directly as reported by the underlying sensor IC.
+            int rawX = modernRoboticsI2cGyro.rawX();
+            int rawY = modernRoboticsI2cGyro.rawY();
+            int rawZ = modernRoboticsI2cGyro.rawZ();
+            int heading = modernRoboticsI2cGyro.getHeading();
+            int integratedZ = modernRoboticsI2cGyro.getIntegratedZValue();
+
+            // Read dimensionalized data from the gyro. This gyro can report angular velocities
+            // about all three axes. Additionally, it internally integrates the Z axis to
+            // be able to report an absolute angular Z orientation.
+            AngularVelocity rates = gyro.getAngularVelocity(AngleUnit.DEGREES);
+            float zAngle = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+
+            // Read administrative information from the gyro
+            int zAxisOffset = modernRoboticsI2cGyro.getZAxisOffset();
+            int zAxisScalingCoefficient = modernRoboticsI2cGyro.getZAxisScalingCoefficient();
+
+            telemetry.addLine()
+                    .addData("dx", formatRate(rates.xRotationRate))
+                    .addData("dy", formatRate(rates.yRotationRate))
+                    .addData("dz", "%s deg/s", formatRate(rates.zRotationRate));
+            telemetry.addData("angle", "%s deg", formatFloat(zAngle));
+            telemetry.addData("heading", "%3d deg", heading);
+            telemetry.addData("integrated Z", "%3d", integratedZ);
+            telemetry.addLine()
+                    .addData("rawX", formatRaw(rawX))
+                    .addData("rawY", formatRaw(rawY))
+                    .addData("rawZ", formatRaw(rawZ));
+            telemetry.addLine().addData("z offset", zAxisOffset).addData("z coeff", zAxisScalingCoefficient);
+            telemetry.update();
+        }
+    }
+
+    String formatRaw(int rawValue) {
+        return String.format("%d", rawValue);
+    }
+
+    String formatRate(float rate) {
+        return String.format("%.3f", rate);
+    }
+
+    String formatFloat(float rate) {
+        return String.format("%.3f", rate);
+    }
+
+}

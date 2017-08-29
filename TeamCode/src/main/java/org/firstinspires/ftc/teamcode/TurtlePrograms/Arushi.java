cb1dbb58ea6c0f7900d5aaba9c7ee08b57b6710b
KetoSharma <31419390+KetoSharma@users.noreply.github.com>
2017-08-29 15:32:37 -0400

    Update Arushi.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtlePrograms/Arushi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtlePrograms/Arushi.java
@@ -28,6 +28,10 @@ public class Autonomous_Template extends LinearOpMode {
        //Put your nice fancy codies here
        
        robot.rightMotor.setPower(1);
+       robot.leftMotor.setPower(1);
+       TimeUnit.SECONDS.sleep(2);
+       robot.rightMotor.setPower(0);
+       robot.leftMotor.setPower(0);
        
     }
 }
